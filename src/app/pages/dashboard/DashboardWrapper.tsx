f92fecf45a429d96db5b394f3565156124e60bfe
ogochukwu <ogochukwu.odom@kodehauz.com>
2021-10-27 16:50:18 +0100

    ch: implement dashboard pages for home-dashboard

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,16 +1,100 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {FC} from 'react'
+import React, {FC, useState } from 'react'
 import {useIntl} from 'react-intl'
 import {PageTitle} from '../../../shared/layout/core'
+import './Dashboard.css'
+import AccountComponent from '../../../components/AccountComponent'
+import ProfileComponent from '../../../components/ProfileComponent'
+import CompanyComponent from '../../../components/CompanyComponent'
+import OwnerIdComponent from '../../../components/OwnerIdComponent'
 
+// const setStep  = () => {
+//   [activeStep, setActiveStep] = useState(0);
+// }
 
-const DashboardPage: FC = () => (
-  <>
-    <div className='row g-5 gx-xxl-8'>
-  
-    </div>
-  </>
-)
+const DashboardPage: FC = () => {
+  const  [activeStep, setActiveStep] = useState(0);
+  return (
+    <>
+      <div className='row g-5 gx-xxl-8'>
+        <div className='d-flex'>
+          <div className='d-card'>
+            <div className='my-5'>
+              <div className='d-flex justify-content-between d-heading my-5'>
+                <div className="form-check">
+                  <label className="form-check-label" htmlFor="flexCheckDefault">
+                    Profile
+                  </label>
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked={activeStep > 0 ? true : false} />
+                </div>
+                <div className="form-check">
+                  <label className="form-check-label" htmlFor="flexCheckDefault">
+                    Company
+                  </label>
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked={activeStep > 1 ? true : false} />
+                </div>
+                <div className="form-check">
+                  <label className="form-check-label" htmlFor="flexCheckDefault">
+                    Owner
+                  </label>
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked={activeStep > 2 ? true : false}/>
+                </div>
+                <div className="form-check">
+                  <label className="form-check-label" htmlFor="flexCheckDefault">
+                    Account
+                  </label>
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked={activeStep > 3 ? true : false} />
+                </div>
+              </div>
+              <div className='d-flex justify-content-end mb-3'>
+                <h6>{activeStep} of 4 steps completed</h6>
+              </div>
+              <div className='card'>
+                <div className='card-body'>
+                  {activeStep === 0 ?
+                    <ProfileComponent setActiveStep={setActiveStep} activeStep={activeStep} />
+                    : activeStep === 1 ?
+                      <CompanyComponent setActiveStep={setActiveStep} activeStep={activeStep} />
+                      : (activeStep === 2) ?
+                        <OwnerIdComponent setActiveStep={setActiveStep} activeStep={activeStep} />
+                        : (activeStep === 3) ?
+                          <AccountComponent setActiveStep={setActiveStep} activeStep={activeStep} />
+                          :
+                          <section>
+                            <h4 className='text-center mb-5'>The following tasks are completed</h4>
+                            <div className='d-flex justify-content-between align-items-center mb-5'>
+                              <h3>Business information</h3>
+                              <button className='btn btn-light'>Edit</button>
+                            </div>
+                            <div className='d-flex justify-content-between align-items-center mb-5'>
+                              <h3>Personal information</h3>
+                              <button className='btn btn-light'>Edit</button>
+                            </div>
+                            <div className='d-flex justify-content-between align-items-center mb-5'>
+                              <h3>Owner Verification</h3>
+                              <button className='btn btn-light'>Edit</button>
+                            </div>
+                            <div className='d-flex justify-content-between align-items-center mb-5 border-bottom-2'>
+                              <h3>Account details</h3>
+                              <button className='btn btn-light'>Edit</button>
+                            </div>
+                            <div className="mb-5 text-center">
+                              <button className='btn-learning btn btn-warning'>Activate Business</button>
+                            </div>
+                            <div className='mb-5 text-center'>
+                              <button className='btn-learning btn btn-light' onClick={() => setActiveStep(activeStep - 1)}>Prev-All Done</button>
+                            </div>
+                          </section>
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
 const DashboardWrapper: FC = () => {
   const intl = useIntl()
