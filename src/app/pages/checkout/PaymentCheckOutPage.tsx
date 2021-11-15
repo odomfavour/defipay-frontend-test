4bfb6e66e0f4a837f4e99a081d2354a29c0a5eed
odomfavour <ogochukwuodom@gmail.com>
2021-11-15 10:28:59 +0100

    ch: improve onetimepayment checkout

--- src/app/pages/checkout/PaymentCheckOutPage.tsx
+++ src/app/pages/checkout/PaymentCheckOutPage.tsx
@@ -1,5 +1,7 @@
+import OneTimePaymentCheckout from '../../modules/checkout/components/OneTimePaymentCheckout'
 import SubscriptionCheckout from '../../modules/checkout/components/SubscriptionCheckout'
 import './checkoutpage.css'
 export function PaymentCheckOutPage() {
-  return <SubscriptionCheckout />
+  return <OneTimePaymentCheckout/>
+  // <SubscriptionCheckout />
 }
